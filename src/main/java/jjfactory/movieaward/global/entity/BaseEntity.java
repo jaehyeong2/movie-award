e01b8bf04bb1547db4c6d59cbbb77c98f0844584
jaehyeong2 <wogud1514@naver.com>
2022-11-03 14:24:59 +0900

    fix : 엔티티리스너 동작 안하는것 수정

--- src/main/java/jjfactory/movieaward/global/entity/BaseEntity.java
+++ src/main/java/jjfactory/movieaward/global/entity/BaseEntity.java
@@ -3,6 +3,7 @@ package jjfactory.movieaward.global.entity;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.config.AuditingBeanDefinitionParser;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -10,7 +11,7 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@EntityListeners(AuditingBeanDefinitionParser.class)
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 public class BaseEntity {

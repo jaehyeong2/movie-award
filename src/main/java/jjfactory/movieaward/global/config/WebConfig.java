f3d8e614e4eb7c168ba75e0f1ce84eaa97c25fcd
jaehyeong2 <wogud1514@naver.com>
2022-11-03 14:25:29 +0900

    feat : swagger 설정 추가

---
+++ src/main/java/jjfactory/movieaward/global/config/WebConfig.java
@@ -0,0 +1,14 @@
+package jjfactory.movieaward.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
+}
